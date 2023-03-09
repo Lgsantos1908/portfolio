627aae237e10b515f41ee8f110947ee49d804058
Luiz Gabriel dos Santos <lgsantos1908@gmail.com>
2023-03-08 21:41:38 -0300

    modificado idade para automatico

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const btnMobile = document.getElementById('btn-mobile');
+const minhaIdade = document.getElementById("idade");
 
 function toggleMenu(event) {
     if (event.type ==='touchstart') event.preventDefault();
@@ -9,3 +10,15 @@ function toggleMenu(event) {
 
 btnMobile.addEventListener('click', toggleMenu)
 btnMobile.addEventListener('touchstart', toggleMenu)
+
+const dataAtual = new Date();
+const anoAtual = dataAtual.getFullYear();
+const mesAtual = dataAtual.getMonth();
+
+if (mesAtual < 8){
+    const minhaIdade = (anoAtual-1) - 1998;
+    idade.innerHTML = minhaIdade;
+}else{
+    const minhaIdade = anoAtual - 1998;
+    idade.innerHTML = minhaIdade;
+}
